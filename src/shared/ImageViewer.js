52560a951fd5f5fadc08747c8c3e96863efdc4ef
Marcos Arthur Brito Lima <cwi.mlima@bv.com.br>
2023-01-15 14:03:06 -0300

    Layout tweaks

--- src/shared/ImageViewer.js
+++ src/shared/ImageViewer.js
@@ -1,3 +1,4 @@
+import '../App.css';
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 function ImageViewer({imagesNames}) {
@@ -8,7 +9,7 @@ function ImageViewer({imagesNames}) {
                 <img 
                     src={require(`../storage/images/${name}.jpeg`)} 
                     alt={name} 
-                    style={{width: "30rem", height: "20rem"}} 
+                    className="App-image"
                 />
             </Carousel.Item>
         ))}
