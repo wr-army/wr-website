0c935feb6bb04a4981862e1e3dda3195cef133f1
Marcos Arthur Brito Lima <cwi.mlima@bv.com.br>
2023-01-15 14:12:14 -0300

    change layout images

--- src/SocialMedia.js
+++ src/SocialMedia.js
@@ -5,8 +5,8 @@ import { FaWhatsapp } from 'react-icons/fa';
 function SocialMedia() {
     return (
         <div className="row">
-            <a className="col" href="https://www.facebook.com/Wallacebatbosareis" target="_blank" without rel="noopener noreferrer"><FaFacebookF /></a>
-            <a className="col" href="https://api.whatsapp.com/send?phone=5547991371927&text=Olá!" target="_blank" without rel="noopener noreferrer"><FaWhatsapp color="green"/></a>
+            <a className="col" href="https://www.facebook.com/Wallacebatbosareis" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
+            <a className="col" href="https://api.whatsapp.com/send?phone=5547991371927&text=Olá!" target="_blank" rel="noopener noreferrer"><FaWhatsapp color="green"/></a>
         </div>
     );
 }
--- src/product/Products.js
+++ src/product/Products.js
@@ -6,7 +6,7 @@ function Products() {
     return (
       <div className='row mt-2'>
         {products.map((product) => (
-          <div key={product.id} className='col'>
+          <div key={product.id} className='col-sm-6 col-xs-12'>
             <div className='card shadow p-3 mb-5 bg-white rounded'>
               <h3>{product.name}</h3>
               <ImageViewer imagesNames={product.images}/>
